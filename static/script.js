4479b374956d39aa01ddf8dd722c5c538e86f53c
Reza75ma <105547804+Reza75ma@users.noreply.github.com>
2023-06-25 12:15:44 +0330

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,64 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var videoElement = document.getElementById('videoElement');
-  var startButton = document.getElementById('startButton');
+// HTML elements
+const videoElement = document.getElementById('video');
+const canvasElement = document.getElementById('canvas');
+const context = canvasElement.getContext('2d');
 
-  // Add click event listener to start button
-  startButton.addEventListener('click', function() {
-    startCamera();
-  });
-
-  function startCamera() {
-    // Request camera permission
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(function(stream) {
-        videoElement.srcObject = stream;
-        startDetection();
-        startButton.style.display = 'none'; // Hide the start button
-      })
-      .catch(function(error) {
-        console.error('Error accessing the webcam:', error);
-      });
+// Get user media (camera) on page load
+window.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    videoElement.srcObject = stream;
+  } catch (error) {
+    console.error('Error accessing camera:', error);
   }
+});
 
-  function startDetection() {
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
+// Draw video frames on canvas
+function drawFrame() {
+  context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+  requestAnimationFrame(drawFrame);
+}
 
-    videoElement.addEventListener('loadedmetadata', function() {
-      canvas.width = videoElement.videoWidth;
-      canvas.height = videoElement.videoHeight;
-    });
-
-    function processFrame() {
-      context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-
-      var src = cv.imread(canvas);
-      var gray = new cv.Mat();
-      cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
-
-      var circles = new cv.Mat();
-      cv.HoughCircles(gray, circles, cv.HOUGH_GRADIENT, 1, 100, 50, 30, 10, 100);
-
-      if (circles.cols > 0) {
-        var circle = circles.data32F;
-        var x = circle[0];
-        var y = circle[1];
-        var radius = circle[2];
-
-        context.beginPath();
-        context.arc(x, y, radius, 0, 2 * Math.PI);
-        context.lineWidth = 2;
-        context.strokeStyle = 'red';
-        context.stroke();
-      }
-
-      gray.delete();
-      circles.delete();
-      src.delete();
-
-      requestAnimationFrame(processFrame);
-    }
-
-    processFrame();
-  }
+// Start drawing frames when video is playing
+videoElement.addEventListener('play', () => {
+  canvasElement.width = videoElement.videoWidth;
+  canvasElement.height = videoElement.videoHeight;
+  drawFrame();
 });
