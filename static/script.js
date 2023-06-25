2f72087293d1b9b8f395e346bbdd7e2617d2710b
Reza75ma <105547804+Reza75ma@users.noreply.github.com>
2023-06-25 18:16:56 +0330

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,9 +1,7 @@
-// HTML elements
-const videoElement = document.getElementById('video');
-
 // Get user media (camera) on page load
 window.addEventListener('DOMContentLoaded', async () => {
   try {
+    const videoElement = document.getElementById('video');
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
     videoElement.srcObject = stream;
     videoElement.play(); // Start playing the video
